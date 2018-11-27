3aca865eb664f9fa9619f4afdbf52622455fcbd8
MGRich <29069561+MGRich@users.noreply.github.com>
2018-11-27 02:17:29 -0600

    have to get that laast commit in before i collapse from exhaustion

--- main/js/header.js
+++
@@ -1,6 +0,0 @@
-$('.dropdown-trigger').dropdown();
-
-document.getElementById("#pdd-btn").addEventListener("click", function() {
-    var instance = M.Dropdown.getInstance(document.getElementById("#pdd-btn"));
-    instance.open();
-})
